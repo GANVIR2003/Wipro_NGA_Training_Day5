c2b4c933aadb66091e8afc79bc90824d88cf53df
Chhavi Ganvir <ganvirchhavi@gmail.com>
2026-01-16 00:08:50 +0530

    Add project files.

---
+++ Day5_Prpgram1/DailyLimitExceeded.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5_Prpgram1
+{
+    public class DailyLimitExceeded :Exception
+    {
+        public DailyLimitExceeded(string message) : base(message)
+        {
+        }
+    }
+
+    class BankAccount
+    {
+        private decimal dailyLimit = 1000;
+        private decimal totalTransactions = 0;
+
+        // Property to get remaining balance
+        public decimal RemainingBalance
+        {
+            get { return dailyLimit - totalTransactions; }
+        }
+
+        public void MakeTransaction(decimal amount)
+        {
+            if (totalTransactions + amount > dailyLimit)
+            {
+                throw new DailyLimitExceeded("Transaction exceeded daily limit.");
+            }
+
+            totalTransactions += amount;
+            Console.WriteLine("Transaction successful.");
+            Console.WriteLine("Remaining balance: " + RemainingBalance);
+        }
+    }
+}
+   
---
+++ Day5_Prpgram1/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Day5_Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("1. Make Transaction");
+                Console.WriteLine("2. Exit");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter transaction amount:");
+                        decimal amount = Convert.ToDecimal(Console.ReadLine());
+
+                        try
+                        {
+                            account.MakeTransaction(amount);
+                        }
+                        catch (DailyLimitExceeded ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine("Remaining balance: " + account.RemainingBalance);
+                        }
+
+                        break;
+
+                    case 2:
+                        exit = true;
+                        Console.WriteLine("Thank you. Exiting application.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Transaction limit Exceeded,Enter your new transaction amount");
+                        break;
+                }
+            }
+        }
+    }
+}
